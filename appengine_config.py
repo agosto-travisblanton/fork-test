95f58071674e68657b32f4b3f8b7e010ee7278dd
Christopher Bartling <chris.bartling@gmail.com>
2015-03-26 16:05:38 -0500

    Checkpoint.

--- appengine_config.py
+++ appengine_config.py
@@ -2,21 +2,64 @@
 The configuration file used by :py:mod:`agar.config` implementations and other libraries using the
 `google.appengine.api.lib_config`_ configuration library. Configuration overrides go in this file.
 """
-from env_setup import setup
-setup()
-
-from agar.env import on_development_server, on_integration_server
-
-##############################################################################
-# AGAR SETTINGS
-##############################################################################
-
-# Root level WSGI application modules that 'agar.url.uri_for()' will search
-agar_url_APPLICATIONS = ['main']
+from env_setup import setup; setup()
+import os
+from agar.env import on_development_server, on_server, on_production_server, on_integration_server
 
 ##############################################################################
-# APPSTATS SETTINGS
+# APPLICATION SETTINGS
 ##############################################################################
 
-appstats_SHELL_OK = on_development_server or on_integration_server
+def _APP_ROOT():
+    return os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+app_APP_ROOT = _APP_ROOT()
+
+
+def _APP_NAME():
+    if on_development_server or on_integration_server or not on_server:
+        return 'skykit-display-device-int'
+    if on_production_server:
+        return 'skykit-display-device'
+    return None
+app_APP_NAME = _APP_NAME()
+
+
+def _SECRET_KEY_FILE():
+    return '{}/privatekeys/{}.pem'.format(app_APP_ROOT, app_APP_NAME)
+app_SECRET_KEY_FILE = _SECRET_KEY_FILE()
+
+def _PRIVATE_KEY():
+    with open(app_SECRET_KEY_FILE) as f:
+        private_key = f.read()
+    return private_key
+app_PRIVATE_KEY = _PRIVATE_KEY()
+
+
+def _SERVICE_ACCOUNT_EMAIL():
+    if on_development_server or on_integration_server or not on_server:
+        return '390010375778-87capuus77kispm64q27iah4kl0rorv4@developer.gserviceaccount.com'
+    if on_production_server:
+        return ''
+    return None
+app_SERVICE_ACCOUNT_EMAIL = _SERVICE_ACCOUNT_EMAIL()
+
+def _CLIENT_ID():
+    if on_development_server or not on_server:
+        return '390010375778-87capuus77kispm64q27iah4kl0rorv4.apps.googleusercontent.com'
+    if on_integration_server:
+        return '390010375778-87capuus77kispm64q27iah4kl0rorv4.apps.googleusercontent.com'
+    if on_production_server:
+        return ''
+    return None
+app_CLIENT_ID = _CLIENT_ID()
+
+# def _CLIENT_SECRET():
+#     if on_development_server or not on_server:
+#         return '5uw_Cj78Iygf3rfnJKZ_SVVO'
+#     if on_integration_server:
+#         return 'h-PGaqnkAfRhjVTtbxcgSLD5'
+#     if on_production_server:
+#         return 'NWCFk0IyE8QPqD2CDwloKvjH'
+#     return None
+# app_CLIENT_SECRET = _CLIENT_SECRET()
 
--- chrome_os_devices_api.py
+++ chrome_os_devices_api.py
@@ -1,10 +1,51 @@
 from google.appengine.api import urlfetch
+import httplib2
+from apiclient import discovery
+from oauth2client.client import SignedJwtAssertionCredentials
+
+from app_config import config
+
 
 __author__ = 'Christopher Bartling <chris.bartling@agosto.com>'
 
 
 class ChromeOsDevicesApi(object):
     """ Facade encapsulating the Directory API of the Admin SDK. """
+
+    DIRECTORY_SERVICE_SCOPES = [
+        'https://www.googleapis.com/auth/admin.directory.device.chromeos'
+    ]
+
+    def __init__(self, domain):
+        self.domain = domain
+        self.admin_user = domain.any_google_admin()
+        self.any_user = self.admin_user if self.admin_user is not None else domain.any_active_user()
+
+    def scopes_enabled(self):
+        impersonate = None
+        view_type = 'domain_public'
+        if self.admin_user:
+            impersonate = self.admin_user.email
+            view_type = 'admin_view'
+        elif self.any_user:
+            impersonate = self.any_user.email
+
+        result = False
+        if impersonate:
+            credentials = SignedJwtAssertionCredentials(config.SERVICE_ACCOUNT_EMAIL,
+                                                        config.PRIVATE_KEY,
+                                                        self.DIRECTORY_SERVICE_SCOPES,
+                                                        sub=impersonate)
+            http_auth = credentials.authorize(httplib2.Http())
+            try:
+                directory_service = discovery.build('admin', 'directory_v1', http=http_auth)
+                # directory_service.users().get(userKey=impersonate, viewType=view_type).execute()
+                result = True
+            except:
+                pass
+
+        return result
+
 
     # https://developers.google.com/admin-sdk/directory/v1/reference/chromeosdevices/list
     def list(self, customer_id):
