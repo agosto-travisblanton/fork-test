a7a07d9fc604a6734c64ba0cffb7fb984967c794
Christopher Bartling <chris.bartling@gmail.com>
2015-04-07 15:10:47 -0500

    Fixed tests.

--- handlers/device_enrollment.py
+++ handlers/device_enrollment.py
@@ -8,8 +8,10 @@ __author__ = 'Christopher Bartling <chris.bartling@agosto.com>'
 
 
 class DeviceEnrollmentHandler(RequestHandler):
+    ADMIN_ACCOUNT_TO_IMPERSONATE = 'administrator@skykit.com'
+
     def get(self):
-        chrome_os_devices_api = ChromeOsDevicesApi()
+        chrome_os_devices_api = ChromeOsDevicesApi(self.ADMIN_ACCOUNT_TO_IMPERSONATE)
         chrome_os_devices = chrome_os_devices_api.list('my_customer')
         self.response.out.write('Hello from skykit-display-device/device registration!')
         json_response(self.response, None)
