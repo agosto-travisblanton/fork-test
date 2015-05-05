08e6e8901fe48eb9db0abaa9f099c995d4db39d2
Christopher Bartling <chris.bartling@gmail.com>
2015-05-05 10:56:38 -0500

    Configuring Express middleware in gulp to allow API proxying back to the App Engine application.

--- webapp/gulp/proxy.js
+++ webapp/gulp/proxy.js
@@ -1,17 +1,17 @@
- /*jshint unused:false */
+/*jshint unused:false */
 
 /***************
 
-  This file allow to configure a proxy system plugged into BrowserSync
-  in order to redirect backend requests while still serving and watching
-  files from the web project
+ This file allow to configure a proxy system plugged into BrowserSync
+ in order to redirect backend requests while still serving and watching
+ files from the web project
 
-  IMPORTANT: The proxy is disabled by default.
+ IMPORTANT: The proxy is disabled by default.
 
-  If you want to enable it, watch at the configuration options and finally
-  change the `module.exports` at the end of the file
+ If you want to enable it, watch at the configuration options and finally
+ change the `module.exports` at the end of the file
 
-***************/
+ ***************/
 
 'use strict';
 
@@ -27,7 +27,7 @@ var proxy = httpProxy.createProxyServer({
   target: proxyTarget
 });
 
-proxy.on('error', function(error, req, res) {
+proxy.on('error', function (error, req, res) {
   res.writeHead(500, {
     'Content-Type': 'text/plain'
   });
@@ -48,20 +48,20 @@ function proxyMiddleware(req, res, next) {
    * for your needs. If you can, you could also check on a context in the url which
    * may be more reliable but can't be generic.
    */
-  if (/\.(html|css|js|png|jpg|jpeg|gif|ico|xml|rss|txt|eot|svg|ttf|woff|woff2|cur)(\?((r|v|rel|rev)=[\-\.\w]*)?)?$/.test(req.url)) {
-    next();
-  } else {
+  if (/\/api\//.test(req.url)) {
+    console.log(chalk.green('[Proxy] ' + req.url));
     proxy.web(req, res);
+  } else {
+    next();
   }
 }
 
-/*
- * This is where you activate or not your proxy.
- *
- * The first line activate if and the second one ignored it
- */
-
-//module.exports = [proxyMiddleware];
+// Enables proxying middleware
 module.exports = function() {
-  return [];
+  return [proxyMiddleware];
 };
+
+// Disables proxying middleware
+//module.exports = function() {
+//  return [];
+//};
