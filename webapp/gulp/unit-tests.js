ad3beea54bd58d4c8e21437827ce1dec5fe57ecf
Christopher Bartling <chris.bartling@gmail.com>
2015-05-11 16:39:57 -0500

    Fixed gulp tests.

--- webapp/gulp/unit-tests.js
+++ webapp/gulp/unit-tests.js
@@ -1,63 +1,13 @@
 'use strict';
 
 var gulp = require('gulp');
-
-var $ = require('gulp-load-plugins')();
-
-var wiredep = require('wiredep');
-var karma = require('karma');
-var concat = require('concat-stream');
-var _ = require('lodash');
-
-module.exports = function(options) {
-  function listFiles(callback) {
-    var wiredepOptions = _.extend({}, options.wiredep, {
-      dependencies: true,
-      devDependencies: true
-    });
-    var bowerDeps = wiredep(wiredepOptions);
-
-    var specFiles = [
-      options.src + '/**/*.spec.js',
-      options.src + '/**/*.mock.js'
-    ];
-
-    var htmlFiles = [
-      options.src + '/**/*.html'
-    ];
-
-    var srcFiles = [
-      options.tmp + '/serve/app/**/*.js'
-    ].concat(specFiles.map(function(file) {
-      return '!' + file;
-    }));
+var karma = require('karma').server;
 
 
-    gulp.src(srcFiles)
-      .pipe($.angularFilesort()).on('error', options.errorHandler('AngularFilesort'))
-      .pipe(concat(function(files) {
-        callback(bowerDeps.js
-          .concat(_.pluck(files, 'path'))
-          .concat(htmlFiles)
-          .concat(specFiles));
-      }));
-  }
+module.exports = function (options) {
 
-  function runTests (singleRun, done) {
-    listFiles(function(files) {
-      karma.server.start({
-        configFile: __dirname + '/../karma.conf.js',
-        files: files,
-        singleRun: singleRun,
-        autoWatch: !singleRun
-      }, done);
-    });
-  }
-
-  gulp.task('test', ['scripts'], function(done) {
-    runTests(true, done);
-  });
-  gulp.task('test:auto', ['watch'], function(done) {
-    runTests(false, done);
+  gulp.task('test', function (done) {
+    karma.start({configFile: __dirname + '/../karma.conf.js', singleRun: true}, done);
   });
 };
+
