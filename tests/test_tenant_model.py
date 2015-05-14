bf54cc57e8b54b0c14d9afa6f475b1336e69c20f
bobmacneal <bob.macneal@workingcompany.com>
2015-05-14 14:39:21 -0500

    Add test to ensure api key is generated

--- tests/test_tenant_model.py
+++ tests/test_tenant_model.py
@@ -7,27 +7,38 @@ __author__ = 'Christopher Bartling <chris.bartling@agosto.com>'
 class TestTenantModel(BaseTest):
     NAME = 'foobar tenant'
     ADMIN_EMAIL = 'foo@bar.com'
-    API_KEY = 'SOME_KEY'
     CONTENT_SERVER_URL = 'https://www.content.com'
+    CHROME_DEVICE_DOMAIN = 'bar.com'
 
     def setUp(self):
         super(TestTenantModel, self).setUp()
 
     def testFindByName_ReturnsMatchingTenant(self):
-        tenant = Tenant(name=self.NAME,
-                        admin_email=self.ADMIN_EMAIL,
-                        content_server_api_key=self.API_KEY,
-                        content_server_url=self.CONTENT_SERVER_URL)
+        tenant = Tenant.create(name=self.NAME,
+                               admin_email=self.ADMIN_EMAIL,
+                               content_server_url=self.CONTENT_SERVER_URL,
+                               chrome_device_domain=self.CHROME_DEVICE_DOMAIN)
         expected_key = tenant.put()
         actual = Tenant.find_by_name(self.NAME)
         self.assertEqual(actual.key, expected_key)
         self.assertEqual(actual.name, self.NAME)
 
     def testFindByName_ReturnsNone_WhenNoMatchingTenantFound(self):
-        tenant = Tenant(name=self.NAME,
-                        admin_email=self.ADMIN_EMAIL,
-                        content_server_api_key=self.API_KEY,
-                        content_server_url=self.CONTENT_SERVER_URL)
+        tenant = Tenant.create(name=self.NAME,
+                               admin_email=self.ADMIN_EMAIL,
+                               content_server_url=self.CONTENT_SERVER_URL,
+                               chrome_device_domain=self.CHROME_DEVICE_DOMAIN)
         tenant.put()
         actual = Tenant.find_by_name('barfood tenant')
         self.assertIsNone(actual)
+
+    def testCreate_AutoGeneratesContentServerApiKey(self):
+        tenant = Tenant.create(name=self.NAME,
+                               admin_email=self.ADMIN_EMAIL,
+                               content_server_url=self.CONTENT_SERVER_URL,
+                               chrome_device_domain=self.CHROME_DEVICE_DOMAIN)
+        tenant.put()
+        tenant_created = Tenant.find_by_name(self.NAME)
+        generated_key = tenant_created.content_server_api_key
+        print "Generated api key: ", generated_key
+        self.assertIsNotNone(generated_key)
