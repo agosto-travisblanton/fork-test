9d08b6ff9264d21a7f0a75860a54669656b4a102
bobmacneal <bob.macneal@workingcompany.com>
2015-05-20 14:38:42 -0500

    Change to 422 bad response

--- handlers/device_control.py
+++ handlers/device_control.py
@@ -11,26 +11,25 @@ class DeviceCommandsHandler(RequestHandler):
         pass
 
     def post(self, device_id):
-        if device_id:
-            chrome_os_device = ChromeOsDevice.get_by_device_id(device_id)
-            if chrome_os_device:
-                try:
-                    if self.request.body is not None:
-                        request_json = json.loads(self.request.body)
-                        command = request_json['command']
-                        payload = request_json['payload']
-                        if command == 'change_channel':
-                            change_channel(chrome_os_device.gcm_registration_id, payload)
-                            self.response.set_status(200)
-                        else:
-                            self.response.set_status(403, 'forbidden command')
-                    self.response.headers.pop('Content-Type', None)
-                except Exception, e:
-                    self.abort(422, 'An error occurred while processing the device command: {0}'.format(e.message))
-            else:
-                self.abort(422, 'Unable to find an existing ChromeOS device entity by device ID: {0}'.format(device_id))
+        chrome_os_device = ChromeOsDevice.get_by_device_id(device_id)
+        self.response.headers.pop('Content-Type', None)
+        if chrome_os_device:
+            try:
+                if self.request.body is not None:
+                    request_json = json.loads(self.request.body)
+                    command = request_json['command']
+                    payload = request_json['payload']
+                    if command == 'change_channel':
+                        change_channel(chrome_os_device.gcm_registration_id, payload)
+                        self.response.set_status(200)
+                    else:
+                        self.response.set_status(422, 'Unrecognized command')
+            except Exception, e:
+                self.response.set_status(422, 'An error occurred while processing the device command: {0}'.
+                                         format(e.message))
         else:
-            self.abort(422, 'A device_id is required to send commands to an existing ChromeOS device entity.')
+            self.response.set_status(422, 'Unable to find an existing ChromeOS device entity by device ID: {0}'.
+                                     format(device_id))
 
     def put(self, device_id):
         pass
--- tests/test_device_control.py
+++ tests/test_device_control.py
@@ -16,25 +16,33 @@ class TestDeviceCommandsHandler(BaseTest, WebTest):
 
     def test_post_known_command_returns_ok_status(self):
         request_parameters = {'command': 'change_channel',
-                              'payload': {'channel': {'name': 'Quality On Demand','program': 'Program 1'}}}
+                              'payload': {'channel': {'name': 'Quality On Demand', 'program': 'Program 1'}}}
         uri = application.router.build(None, 'device-commands', None, {'device_id': self.device_id})
         response = self.app.post_json(uri, params=request_parameters)
         self.assertOK(response)
 
-    def test_post_unknown_command_returns_forbidden_status(self):
-        request_parameters = {'command': 'unknown',
+    def test_post_unknown_command_returns_422_error(self):
+        request_parameters = {'command': 'foobar',
                               'payload': {}}
         uri = application.router.build(None, 'device-commands', None, {'device_id': self.device_id})
-        response = self.app.post_json(uri, params=request_parameters, expect_errors=True)
-        self.assertForbidden(response)
         with self.assertRaises(Exception) as context:
             self.app.post_json(uri, params=request_parameters)
-        self.assertTrue('Bad response: 403 forbidden command' in str(context.exception))
+        self.assertTrue('Bad response: 422 Unrecognized command' in str(context.exception))
 
     def test_post_invokes_processor_method(self):
-        payload = {'channel': {'name': 'Quality On Demand','program': 'Program 1'}}
+        payload = {'channel': {'name': 'Quality On Demand', 'program': 'Program 1'}}
         request_parameters = {'command': 'change_channel', 'payload': payload}
         uri = application.router.build(None, 'device-commands', None, {'device_id': self.device_id})
         with patch('device_commands_processor.change_channel', return_value=None) as mock_command:
             self.app.post_json(uri, params=request_parameters)
         mock_command.assert_called_once_with(self.chrome_os_device.gcm_registration_id, payload)
+
+    def test_post_unknown_device_returns_422_error(self):
+        request_parameters = {'command': 'change_channel',
+                              'payload': {}}
+        uri = application.router.build(None, 'device-commands', None, {'device_id': 'foobar'})
+        with self.assertRaises(Exception) as context:
+            self.app.post_json(uri, params=request_parameters)
+        self.assertTrue('Bad response: 422 Unable to find an existing ChromeOS device entity by device ID: foobar'
+                        in str(context.exception))
+
