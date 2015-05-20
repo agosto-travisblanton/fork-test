b31ae22d4fbaa5f0e35f2185171793bf3a634ac3
bobmacneal <bob.macneal@workingcompany.com>
2015-05-20 13:08:00 -0500

    Refactor test

--- device_commands_processor.py
+++ device_commands_processor.py
@@ -3,6 +3,13 @@ from google_cloud_messaging import GoogleCloudMessaging
 __author__ = 'Christopher Bartling <chris.bartling@agosto.com>'
 
 
+def change_channel(gcm_registration_id, payload):
+    registration_ids = [gcm_registration_id]
+    data_dictionary = {'command': 'change_channel', 'payload': payload}
+    google_cloud_messaging = GoogleCloudMessaging()
+    google_cloud_messaging.notify(registration_ids, data_dictionary, test_mode=False)
+
+
 def register_device():
     pass
 
@@ -19,9 +26,4 @@ def check_schedule():
     pass
 
 
-def change_channel(gcm_registration_id, payload):
-    registration_ids = [gcm_registration_id]
-    data_dictionary = {'command': 'change_channel', 'payload': payload}
-    google_cloud_messaging = GoogleCloudMessaging()
-    google_cloud_messaging.notify(registration_ids, data_dictionary, test_mode=False)
 
