e123e3b7a99b21b6c7225279c1e085977df17f86
Aaron Olson <aaron.olson@agosto.com>
2015-05-28 15:29:08 -0500

    SD-540 Wrote selenium tests for adding editing, and deleting tenants. Added the ability to run tests locally using the chrome webdriver to be able to view tests while they run.

--- functional_tests/test_add_device.py
+++ functional_tests/test_add_device.py
@@ -1,6 +1,8 @@
 import unittest
 from func_test_config import BaseTest
 
+__author__ = 'Aaron Olson <aaron.olson@agosto.com>'
+
 class AddDevice(BaseTest):
 
     def test_add_device(self):
@@ -14,13 +16,9 @@ class AddDevice(BaseTest):
         driver.find_element_by_id("navbar-domains").click()
         driver.find_element_by_id("skykit-domain").click()
         driver.find_element_by_id("button-add-device").click()
-        driver.find_element_by_id("device-name").clear()
         driver.find_element_by_id("device-name").send_keys("R2D2")
-        driver.find_element_by_id("device-location").clear()
         driver.find_element_by_id("device-location").send_keys("Space")
-        driver.find_element_by_id("mac-address").clear()
         driver.find_element_by_id("mac-address").send_keys("000:875:123")
-        driver.find_element_by_id("device-id").clear()
         driver.find_element_by_id("device-id").send_keys("1234567890")
         driver.find_element_by_xpath("//button[@type='submit']").click()
         driver.find_element_by_css_selector("button.confirm").click()
