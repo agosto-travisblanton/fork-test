f0e9ff988305c9edabf1e5cefe1549baaa90da3d
Aaron Olson <aaron.olson@agosto.com>
2015-06-01 11:43:19 -0500

    SD-451 Added a content change command to  pass to the content change notification handler. Also created a unittest for it.

--- device_commands_processor.py
+++ device_commands_processor.py
@@ -9,6 +9,12 @@ def change_channel(gcm_registration_id, payload):
     google_cloud_messaging = GoogleCloudMessaging()
     google_cloud_messaging.notify(registration_ids, data_dictionary, test_mode=False)
 
+def content_change_notification(gcm_registration_id, payload):
+    registration_ids = [gcm_registration_id]
+    data_dictionary = {'command': 'content_change', 'payload': payload}
+    google_cloud_messaging = GoogleCloudMessaging()
+    google_cloud_messaging.notify(registration_ids, data_dictionary, test_mode=False)
+
 
 def register_device():
     pass
@@ -24,6 +30,3 @@ def update_device():
 
 def check_schedule():
     pass
-
-
-
--- tests/test_device_commands_processor.py
+++ tests/test_device_commands_processor.py
@@ -1,7 +1,7 @@
 from agar.test import BaseTest
 from mock import patch
 
-from device_commands_processor import (change_channel)
+from device_commands_processor import (change_channel, content_change_notification)
 
 __author__ = 'Bob MacNeal <bob.macneal@agosto.com>'
 
@@ -20,3 +20,12 @@ class TestDeviceCommandsProcessor(BaseTest):
             change_channel(gcm_registration_id, payload)
         mock_notify.assert_called_once_with(registration_ids, data_dictionary, test_mode=False)
 
+    @staticmethod
+    def test_content_change_notification():
+        payload = {'content': {'name': 'Game of Thrones Slide Show', 'program': 'Program 1'}}
+        gcm_registration_id = 'd8765685748456vgd456745e54'
+        registration_ids = [gcm_registration_id]
+        data_dictionary = {'command': 'content_change', 'payload': payload}
+        with patch('google_cloud_messaging.GoogleCloudMessaging.notify') as mock_notify:
+            content_change_notification(gcm_registration_id, payload)
+        mock_notify.assert_called_once_with(registration_ids, data_dictionary, test_mode=False)
