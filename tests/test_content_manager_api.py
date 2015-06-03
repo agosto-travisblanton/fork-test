d8fda54e437a19dbece71a4332afdcc5d93ef021
bobmacneal <bob.macneal@workingcompany.com>
2015-06-03 13:38:45 -0500

    Add test harness

---
+++ tests/test_content_manager_api.py
@@ -0,0 +1,17 @@
+__author__ = 'Bob MacNeal <bob.macneal@agosto.com>'
+
+from env_setup import setup_test_paths;
+
+setup_test_paths()
+
+from agar.test import BaseTest
+from content_manager_api import ContentManagerApi
+
+
+class TestContentManagerApi(BaseTest):
+    def setUp(self):
+        super(TestContentManagerApi, self).setUp()
+        self.content_manager_api = ContentManagerApi(self.ADMIN_ACCOUNT_TO_IMPERSONATE)
+
+    def test_create_tenant(self):
+        pass
