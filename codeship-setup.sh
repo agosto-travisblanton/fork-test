e0f802f9595ce6e81efa2221b8b6325da71106d1
Christopher Bartling <chris.bartling@gmail.com>
2015-09-21 22:16:53 -0500

    Setting up node.js and npm.

--- codeship-setup.sh
+++ codeship-setup.sh
@@ -3,6 +3,10 @@
 COLOR_LIGHT_GREEN='\033[1;32m'
 COLOR_OFF='\033[0m'
 
-printf "\n${COLOR_LIGHT_GREEN}===> Installing Node and NVM...${COLOR_OFF}\n"
+NODE_VERSION=4.1.0
 
+printf "\n${COLOR_LIGHT_GREEN}===> Installing node.js ${NODE_VERSION} via nvm...${COLOR_OFF}\n"
+nvm install $NODE_VERSION
 
+printf "\n${COLOR_LIGHT_GREEN}===> Updating npm to the latest...${COLOR_OFF}\n"
+npm install -g npm@latest
