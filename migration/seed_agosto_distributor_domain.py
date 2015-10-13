2258c31ed89eb6df67f852ae1e14445d833c03bb
bobmacneal <bob.macneal@workingcompany.com>
2015-10-13 15:32:42 -0500

    Add tierney to distributor migration

--- migration/seed_agosto_distributor_domain.py
+++ migration/seed_agosto_distributor_domain.py
@@ -8,6 +8,7 @@ __author__ = 'Bob MacNeal <bob.macneal@agosto.com>'
 class SeedAgostoDistributorDomain(MigrationBase):
     MIGRATION_NAME = 'seed_agosto_distributor_domain'
     AGOSTO_DISTRIBUTOR_NAME = 'Agosto'
+    TIERNEY_DISTRIBUTOR_NAME = 'Tierney Bros., Inc.'
 
     def __init__(self):
         super(SeedAgostoDistributorDomain, self).__init__(self.MIGRATION_NAME)
@@ -16,6 +17,7 @@ class SeedAgostoDistributorDomain(MigrationBase):
         agosto_distributor = Distributor.find_by_name(self.AGOSTO_DISTRIBUTOR_NAME)
         if agosto_distributor is None:
             agosto_distributor = Distributor.create(name=self.AGOSTO_DISTRIBUTOR_NAME, active=True)
+            agosto_distributor.admin_email = 'admin@agosto.com'
             agosto_distributor.put()
         default_domain = config.DEFAULT_AGOSTO_DEVICE_DOMAIN.lower()
         agosto_default_domain = Domain.find_by_name(default_domain)
@@ -26,3 +28,9 @@ class SeedAgostoDistributorDomain(MigrationBase):
                               impersonation_admin_email_address=config.IMPERSONATION_ADMIN_EMAIL_ADDRESS,
                               active=True)
             agosto_default_domain.put()
+
+        tierney_distributor = Distributor.find_by_name(self.TIERNEY_DISTRIBUTOR_NAME)
+        if tierney_distributor is None:
+            tierney_distributor = Distributor.create(name=self.TIERNEY_DISTRIBUTOR_NAME, active=True)
+            tierney_distributor.admin_email = 'admin@agosto.com'
+            tierney_distributor.put()
