a99708bbb31a1b08f556303fbe86b6a1e0c762a8
bobmacneal <bob.macneal@workingcompany.com>
2015-11-04 11:37:49 -0600

    Rename property in gcm payload

--- device_message_processor.py
+++ device_message_processor.py
@@ -24,7 +24,7 @@ def change_intent(gcm_registration_id, payload):
 def send_unmanaged_device_info(gcm_registration_id, device_urlsafe_key):
     registration_ids = [gcm_registration_id]
     data_dictionary = dict(deviceKey=device_urlsafe_key, unmanagedApiToken=config.UNMANAGED_API_TOKEN)
-Re    google_cloud_messaging = GoogleCloudMessaging()
+    google_cloud_messaging = GoogleCloudMessaging()
     test_mode = config.GCM_TEST_MODE
     try:
         google_cloud_messaging.notify(registration_ids, data_dictionary, test_mode=test_mode)
