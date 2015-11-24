20cd2b8c4efe864767e54485a43a8a1e19a720ab
bobmacneal <bob.macneal@workingcompany.com>
2015-11-24 17:09:34 -0600

    Update console script

--- scripts/initialize_device_heartbeat_for_all_devices.py
+++ scripts/initialize_device_heartbeat_for_all_devices.py
@@ -1,24 +1,16 @@
 # Console script to initialize heartbeats for all devices
-from models import ChromeOsDevice, DeviceHeartbeat
+from datetime import datetime
+
+from models import ChromeOsDevice
 
 query = ChromeOsDevice.query().order(ChromeOsDevice.created)
 devices = query.fetch(1000)
 print 'Device count = ' + str(len(devices))
-query = DeviceHeartbeat.query().order(DeviceHeartbeat.created)
-beats = query.fetch(1000)
-print 'Initial beat count = ' + str(len(beats))
 for device in devices:
-    beat = DeviceHeartbeat.find_by_device_key(device.key)
-    if None == beat:
+    if None == device.heartbeat_updated or len(str(device.heartbeat_updated)) == 0:
         print 'Setting beat for device with key and mac_address: ' + device.key.urlsafe() + ', ' + device.mac_address
-        beat = DeviceHeartbeat.create(device_key=device.key, disk_utilization=0, memory_utilization=0,
-                                      program_playing='****initial****')
-        beat.put()
-
-query = DeviceHeartbeat.query().order(DeviceHeartbeat.created)
-beats = query.fetch(1000)
-for beat in beats:
-    print beat.device_key.urlsafe() + ' playing: "' + beat.program_playing + '". (' + str(beat.updated) + ')'
-query = DeviceHeartbeat.query().order(DeviceHeartbeat.created)
-beats = query.fetch(1000)
-print 'Final beat count = ' + str(len(beats))
+        device.disk_utilization = 0
+        device.memory_utilization = 0
+        device.heartbeat_updated = datetime.utcnow()
+        device.program_player = '****initial****'
+        device.put()
