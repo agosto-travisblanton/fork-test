970d7d857ac6d308be3f5d33981334118d46edf8
bobmacneal <bob.macneal@workingcompany.com>
2016-01-14 13:58:28 -0600

    Update sample data

--- scripts/populate_player_command_events_with_sample_data.py
+++ scripts/populate_player_command_events_with_sample_data.py
@@ -6,10 +6,25 @@ print 'Device count = ' + str(len(devices))
 i = 0
 for device in devices:
     i += 1
-    payload = 'some-command-{0}'.format(i)
+    payload = 'some-command'.format(i)
     gcm_registration_id = 'gcm-registration-id-{0}'.format(i)
     event = PlayerCommandEvent.create(device_urlsafe_key=device.key.urlsafe(),
-                                    payload=payload, gcm_registration_id=gcm_registration_id)
+                                      payload=payload, gcm_registration_id=gcm_registration_id)
 
+    event.put()
+    print 'Added ' + payload + ' event to ' + device.key.urlsafe()
+    payload = 'another-command'.format(i)
+    gcm_registration_id = 'gcm-registration-id-{0}'.format(i)
+    event = PlayerCommandEvent.create(device_urlsafe_key=device.key.urlsafe(),
+                                      payload=payload, gcm_registration_id=gcm_registration_id)
+    event.player_has_confirmed = True
+    event.put()
+    print 'Added ' + payload + ' event to ' + device.key.urlsafe()
+    payload = 'yet-another-command'.format(i)
+    gcm_registration_id = 'gcm-registration-id-{0}'.format(i)
+    event = PlayerCommandEvent.create(device_urlsafe_key=device.key.urlsafe(),
+                                      payload=payload, gcm_registration_id=gcm_registration_id)
+
+    event.player_has_confirmed = True
     event.put()
     print 'Added ' + payload + ' event to ' + device.key.urlsafe()
