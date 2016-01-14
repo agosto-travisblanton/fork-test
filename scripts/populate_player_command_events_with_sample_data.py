0d1326d82a08b291cea75e617b996616c5fb7147
bobmacneal <bob.macneal@workingcompany.com>
2016-01-14 12:46:35 -0600

    Add script to populate some test player command event data

---
+++ scripts/populate_player_command_events_with_sample_data.py
@@ -0,0 +1,15 @@
+from models import ChromeOsDevice, PlayerCommandEvent
+
+query = ChromeOsDevice.query().order(ChromeOsDevice.created)
+devices = query.fetch(5)
+print 'Device count = ' + str(len(devices))
+i = 0
+for device in devices:
+    i += 1
+    payload = 'some-command-{0}'.format(i)
+    gcm_registration_id = 'gcm-registration-id-{0}'.format(i)
+    event = PlayerCommandEvent.create(device_urlsafe_key=device.key.urlsafe(),
+                                    payload=payload, gcm_registration_id=gcm_registration_id)
+
+    event.put()
+    print 'Added ' + payload + ' event to ' + device.key.urlsafe()
