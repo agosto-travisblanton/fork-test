a936037b1897758846f26dda70f3c48fa5e260fc
bobmacneal <bob.macneal@workingcompany.com>
2016-01-25 14:09:12 -0600

    Add a seed device geoLocation script

---
+++ scripts/seed_device_geo_location.py
@@ -0,0 +1,7 @@
+__author__ = 'Bob MacNeal <bob.macneal@agosto.com>'
+from models import ChromeOsDevice
+from google.appengine.ext import ndb
+device = ChromeOsDevice.query(ChromeOsDevice.api_key == '1c562bdcfe0e4396a74f8fa470c04f53').get()
+device.geoLocation = ndb.GeoPt(44.983579,-93.277544)
+device.put()
+print 'Device seeded with geoLocation'
