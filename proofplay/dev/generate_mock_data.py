a3ea4ef21ab01c927beca0061f5a981de323172a
Daniel Ternyak <daniel.ternyak@agosto.com>
2016-03-07 15:03:13 -0600

    improved mock data script

--- proofplay/dev/generate_mock_data.py
+++ proofplay/dev/generate_mock_data.py
@@ -4,6 +4,8 @@ import datetime
 import random
 import uuid
 
+analytics = {}
+
 information_to_post = {
     "myacmeservices": {
         "resource_choices": [
@@ -19,40 +21,87 @@ information_to_post = {
                 "acme-one-device": {
                     "serial_number": uuid.uuid1().hex,
                     "device_key": uuid.uuid1().hex,
-                    "new_customer_location_code": "acme-super-one-location",
-                    "new_customer_display_code": "acme-super-one-device"
+                    "new_customer_location_code": "acme-new-one-location",
+                    "new_customer_display_code": "acme-new-one-device"
+                },
+                "acme-two-device": {
+                    "serial_number": uuid.uuid1().hex,
+                    "device_key": uuid.uuid1().hex,
+                    "new_customer_location_code": "acme-new-one-location",
+                    "new_customer_display_code": "acme-new-two-device"
+                }
+            },
+            "shared-two-location": {
+                "acme-three-device": {
+                    "serial_number": uuid.uuid1().hex,
+                    "device_key": uuid.uuid1().hex,
+                    "new_customer_location_code": "shared-new-two-location",
+                    "new_customer_display_code": "acme-new-three-device"
+                },
+                "acme-four-device": {
+                    "serial_number": uuid.uuid1().hex,
+                    "device_key": uuid.uuid1().hex,
+                    "new_customer_location_code": "shared-new-two-location",
+                    "new_customer_display_code": "acme-new-four-device"
+                },
+                "acme-five-device": {
+                    "serial_number": uuid.uuid1().hex,
+                    "device_key": uuid.uuid1().hex,
+                    "new_customer_location_code": "shared-new-two-location",
+                    "new_customer_display_code": "acme-new-five-device"
                 }
             }
         }
 
     },
-    "dilbertdistribution": {
+    "davedistribution": {
+        "resource_choices": [
+            {"name": "DILB_5553", "id": uuid.uuid1().hex},
+            {"name": "DILB_5447", "id": uuid.uuid1().hex},
+            {"name": "DILB_2222", "id": uuid.uuid1().hex},
+            {"name": "DILB_5533", "id": uuid.uuid1().hex},
+            {"name": "DILB_4334", "id": "3333"},
+            {"name": "DILB_4334-V2", "id": "3333"}  # to show that name can change
+        ],
         "locations": {
-            "dilbert-one-location": {
-
-            }
-        }
-
-    },
-    "johnsonsjaguars": {
-        "locations": {
-            "johnson-one-location": {
-
+            "dave-one-location": {
+                "dave-one-device": {
+                    "serial_number": uuid.uuid1().hex,
+                    "device_key": uuid.uuid1().hex,
+                    "new_customer_location_code": "dave-new-one-location",
+                    "new_customer_display_code": "dave-new-one-device"
+                },
+                "dave-two-device": {
+                    "serial_number": uuid.uuid1().hex,
+                    "device_key": uuid.uuid1().hex,
+                    "new_customer_location_code": "dave-new-one-location",
+                    "new_customer_display_code": "dave-new-two-device"
+                }
+            },
+            "shared-two-location": {
+                "dave-three-device": {
+                    "serial_number": uuid.uuid1().hex,
+                    "device_key": uuid.uuid1().hex,
+                    "new_customer_location_code": "shared-new-two-location",
+                    "new_customer_display_code": "dave-new-three-device"
+                },
+                "dave-four-device": {
+                    "serial_number": uuid.uuid1().hex,
+                    "device_key": uuid.uuid1().hex,
+                    "new_customer_location_code": "shared-new-two-location",
+                    "new_customer_display_code": "dave-new-four-device"
+                },
+                "dave-five-device": {
+                    "serial_number": uuid.uuid1().hex,
+                    "device_key": uuid.uuid1().hex,
+                    "new_customer_location_code": "shared-new-two-location",
+                    "new_customer_display_code": "dave-new-five-device"
+                }
             }
         }
 
     }
 }
-
-
-# TODO
-# REQUIREMENTS:
-# MULTIPLE TENANTS #
-# CHANGING customer_location_codes
-# CHANGING customer_display_codes
-# CHANGING resource_names
-# NONE customer_display_codes
-# NONE customer_location_codes
 
 
 def send_mock_data(to_send, integration=False):
@@ -65,33 +114,114 @@ def send_mock_data(to_send, integration=False):
     r = requests.post(url, data=json.dumps(to_send), headers=headers)
 
 
-def create_one_log(started_at, ended_at, tenant):
-    should_make_device_none = random.randint(1, 30)
-    should_make_location_none = random.randint(1, 30)
+def create_one_log(
+        started_at,
+        ended_at,
+        tenant,
+        should_use_no_location_code,
+        should_use_no_display_code,
+        should_use_new_location_code,
+        should_use_new_display_code
+):
+    # array resource dicts
+    tenant_resources = information_to_post[tenant]["resource_choices"]
 
-    to_return = {
-        'resource_name': '',  #
-        'resource_id': '',  #
-        'device_key': '',  #
-        'serial_number': '',  #
-        'customer_location_code': '',  #
-        'customer_display_code': '',  #
+    # dict name and id of resource
+    chosen_resource = random.choice(tenant_resources)
+
+    # dict tenant locations
+    tenant_locations = information_to_post[tenant]["locations"]
+
+    # string of location
+    chosen_location = random.choice(tenant_locations.keys())
+
+    # string chosen device
+    chosen_device = random.choice(information_to_post[tenant]["locations"][chosen_location].keys())
+
+    # json chosen device
+    full_chosen_device = information_to_post[tenant]["locations"][chosen_location][chosen_device]
+
+    customer_display_code = chosen_device
+    customer_location_code = chosen_location
+
+    if should_use_no_location_code:
+        customer_location_code = None
+
+    if should_use_no_display_code:
+        customer_display_code = None
+
+    if should_use_new_location_code:
+        customer_location_code = full_chosen_device["new_customer_location_code"]
+
+    if should_use_new_display_code:
+        customer_display_code = full_chosen_device["new_customer_display_code"]
+
+    if tenant not in analytics:
+        analytics[tenant] = {}
+    if customer_location_code not in analytics[tenant]:
+        analytics[tenant][customer_location_code] = {}
+    if customer_display_code not in analytics[tenant][customer_location_code]:
+        analytics[tenant][customer_location_code][customer_display_code] = {}
+
+    if chosen_resource["name"] not in analytics[tenant][customer_location_code][customer_display_code]:
+        analytics[tenant][customer_location_code][customer_display_code][chosen_resource["name"]] = 0
+
+    analytics[tenant][customer_location_code][customer_display_code][chosen_resource["name"]] += 1
+
+    return {
+        'resource_name': chosen_resource["name"],  #
+        'resource_id': chosen_resource["id"],  #
+        'device_key': full_chosen_device["device_key"],  #
+        'serial_number': full_chosen_device["serial_number"],  #
+        'customer_location_code': customer_location_code,  #
+        'customer_display_code': customer_display_code,  #
         'started_at': started_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
         'ended_at': ended_at.strftime('%Y-%m-%dT%H:%M:%S.%fZ'),
         'tenant_code': tenant
     }
 
-    return to_return
 
-
-def batch_up_logs_for_tenant(amount_a_day, started_at, tenant):
+def batch_up_logs_for_tenant(
+        amount_a_day,
+        started_at,
+        tenant,
+        total_number_of_days,
+        number_of_days_before_today
+):
     to_return = {
         "data": []
     }
 
+    decimal_of_way_through = 1.0 - (number_of_days_before_today / float(total_number_of_days))
+
+    should_use_no_location_code = False
+    should_use_no_display_code = False
+    should_use_new_location_code = False
+    should_use_new_display_code = False
+
+    if decimal_of_way_through <= 0.25:
+        should_use_no_location_code = False
+        should_use_no_display_code = True
+
+    elif decimal_of_way_through >= 0.25 and decimal_of_way_through <= 0.75:
+        should_use_no_location_code = False
+        should_use_no_display_code = False
+
+    else:
+        should_use_new_location_code = True
+        should_use_new_display_code = True
+
     for item in xrange(1, amount_a_day):
         ended_at = started_at + datetime.timedelta(minutes=10)
-        one_log = create_one_log(started_at, ended_at, tenant)
+        one_log = create_one_log(
+            started_at,
+            ended_at,
+            tenant,
+            should_use_no_location_code=should_use_no_location_code,
+            should_use_no_display_code=should_use_no_display_code,
+            should_use_new_location_code=should_use_new_location_code,
+            should_use_new_display_code=should_use_new_display_code
+        )
         to_return["data"].append(one_log)
         started_at += datetime.timedelta(minutes=10)
 
@@ -107,5 +237,13 @@ def generate_mock_data(days_of_logs, amount_a_day):
 
             # to not post some 1/10 days
             if not random.randint(1, 10) == 5:
-                batched_up_logs = batch_up_logs_for_tenant(amount_a_day, started_at, key)
+                batched_up_logs = batch_up_logs_for_tenant(amount_a_day, started_at, key, days_of_logs, each)
                 send_mock_data(batched_up_logs)
+
+    print "THE FOLLOWING IS ANALYTICS ABOUT YOUR POSTED DATA"
+    print analytics
+
+
+generate_mock_data(500, 10)
+
+
--- proofplay/dev/run_int_mock_data.py
+++
@@ -1,3 +0,0 @@
-from make_mock_data import queue_up_mock_data
-
-queue_up_mock_data(500, 10, True)
