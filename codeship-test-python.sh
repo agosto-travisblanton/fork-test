0fae72096075025700bf363379964231eb08e258
Daniel Ternyak <daniel.ternyak@agosto.com>
2016-04-06 12:14:40 -0500

    test just python

--- codeship-test-python.sh
+++ codeship-test-python.sh
@@ -9,6 +9,7 @@ COLOR_OFF='\033[0m'
 printf "\n${COLOR_LIGHT_GREEN}===> Installing Python packages via pip...${COLOR_OFF}\n"
 pip install PyOpenSSL
 pip install py
+pip install -r requirements.txt
 
 printf "\n${COLOR_LIGHT_GREEN}===> Configuring AppEngine SDK environment...${COLOR_OFF}\n"
 
@@ -16,5 +17,4 @@ export APPENGINE_SDK=/home/rof/appengine/python_appengine
 
 printf "\n${COLOR_LIGHT_GREEN}===> Running Python tests...${COLOR_OFF}\n"
 
-ls
 ./manage.py pytest --cov-report=term --cov-report=html --cov=. tests/
