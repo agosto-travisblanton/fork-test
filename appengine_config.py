62be26f81122f07e6151ee6973239233929b3843
Daniel Ternyak <daniel.ternyak@agosto.com>
2016-04-15 15:12:42 -0500

    change back to regular

--- appengine_config.py
+++ appengine_config.py
@@ -414,8 +414,7 @@ def _SQLALCHEMY_DATABASE_URI():
         return db_uri
     else:
         if on_integration_server:
-            db_uri = 'mysql+mysqldb://root:MPclyl0VK5lu@104.154.74.153:3306/provisioning'
-            return db_uri
+            instance_name = "provisioning-int-2"
         elif on_stage_server:
             instance_name = 'provisioning-stage'
         elif on_gamestop_server:
