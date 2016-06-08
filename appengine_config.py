727ce6126d360dd43088974248f21f24ee9ad234
Christopher Bartling <chris.bartling@gmail.com>
2016-06-08 13:22:21 -0500

    Now have settings page coming up.

--- appengine_config.py
+++ appengine_config.py
@@ -309,7 +309,7 @@ app_WIFI_CONNECTION = 'WiFi'
 
 app_INTEGRATION_EVENTS_DEFAULT_FETCH_SIZE = 200
 
-backup_BACKUP_SENDER = 'Backup Datastore Service gcp.admin@agosto.com'
+backup_BACKUP_EMAIL_SENDER = 'Backup Datastore Service <gcp.admin@agosto.com>'
 
 
 def _DEFAULT_CONTENT_MANAGER_URL():
