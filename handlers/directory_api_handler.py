dedef7e0edf9eb1089907f7bd72bf574cebc36d9
bobmacneal <bob.macneal@workingcompany.com>
2016-07-19 15:16:00 -0500

    Remove unneeded references

--- handlers/directory_api_handler.py
+++ handlers/directory_api_handler.py
@@ -1,26 +1,5 @@
-import json
-import logging
-
-from datetime import datetime
-from google.appengine.ext import ndb
-from google.appengine.ext.deferred import deferred
 from webapp2 import RequestHandler
 
-from app_config import config
-from content_manager_api import ContentManagerApi
-from decorators import requires_api_token, requires_registration_token, requires_unmanaged_registration_token
-from device_message_processor import post_unmanaged_device_info, change_intent
-from model_entities.integration_events_log_model import IntegrationEventLog
-from models import ChromeOsDevice, Tenant, Domain, TenantEntityGroup, DeviceIssueLog
-from ndb_mixins import PagingListHandlerMixin, KeyValidatorMixin
-from restler.serializers import json_response
-from strategy import CHROME_OS_DEVICE_STRATEGY, DEVICE_PAIRING_CODE_STRATEGY, DEVICE_ISSUE_LOG_STRATEGY
-from utils.email_notify import EmailNotify
-from utils.timezone_util import TimezoneUtil
-from workflow.refresh_device import refresh_device
-from workflow.refresh_device_by_mac_address import refresh_device_by_mac_address
-from workflow.register_device import register_device
-from workflow.update_chrome_os_device import update_chrome_os_device
 from workflow.interrogate_chrome_os_device_by_mac_address import interrogate_chrome_os_device_by_mac_address
 
 __author__ = 'Bob MacNeal <bob.macneal@agosto.com>'
