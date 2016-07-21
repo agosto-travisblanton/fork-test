56746282afa01fa7d6c93e9b7d4fec0478ed38b4
Daniel Ternyak <daniel.ternyak@agosto.com>
2016-07-20 23:24:47 -0500

    update codeship test

--- codeship-test-javascript.sh
+++ codeship-test-javascript.sh
@@ -14,16 +14,9 @@ printf "\n${COLOR_LIGHT_GREEN}===> Installing node runtime via nvm...${COLOR_OFF
 nvm install $NODEJS_RUNTIME_VERSION
 nvm use $NODEJS_RUNTIME_VERSION
 
-printf "\n${COLOR_LIGHT_GREEN}===> Installing installing the latest version of bower...${COLOR_OFF}\n"
-npm install -g bower@latest
-
 printf "\n${COLOR_LIGHT_GREEN}===> Installing node packages via npm...${COLOR_OFF}\n"
 npm install
 
-printf "\n${COLOR_LIGHT_GREEN}===> Installing web packages via bower...${COLOR_OFF}\n"
-
-bower install
-
 printf "\n${COLOR_LIGHT_GREEN}===> Running Jasmine tests in gulp build...${COLOR_OFF}\n"
 
-gulp test
+karma test
