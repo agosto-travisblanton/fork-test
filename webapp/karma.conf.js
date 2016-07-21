a5a69030c7387a54adc1746bce8842778609c3ec
Daniel Ternyak <daniel.ternyak@agosto.com>
2016-07-21 00:04:39 -0500

    karma progress module

--- webapp/karma.conf.js
+++ webapp/karma.conf.js
@@ -70,7 +70,7 @@ module.exports = function (config) {
     },
 
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['mocha'],
+    reporters: ['progress'],
 
     // web server port
     port: 9876,
