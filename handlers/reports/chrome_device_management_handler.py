232bdfe8f0dd7953992892420c6f6436f580f123
bobmacneal <bob.macneal@workingcompany.com>
2016-08-04 11:34:03 -0500

    Add report route and chrome device management handler

--- handlers/reports/chrome_device_management_handler.py
+++ handlers/reports/chrome_device_management_handler.py
@@ -5,9 +5,9 @@ from workflow.interrogate_chrome_os_device_by_mac_address import interrogate_chr
 __author__ = 'Bob MacNeal <bob.macneal@agosto.com>'
 
 
-class InterrogateDirectoryApiHandler(RequestHandler):
+class ChromeDeviceManagementHandler(RequestHandler):
 
-    def lookup_device_by_parameter(self):
+    def get_by_parameters(self):
         impersonation_email = self.request.get('impersonationEmail')
         device_mac_address = self.request.get('macAddress')
         if device_mac_address and impersonation_email:
@@ -16,4 +16,12 @@ class InterrogateDirectoryApiHandler(RequestHandler):
         else:
             message = 'Missing information in request body.'
             return self.response.set_status(400, message)
+        self.response.set_status(200, 'OK')
+
+    def get_list(self):
+        impersonation_email = self.request.get('impersonationEmail')
+        self.response.set_status(200, 'OK')
+
+    def get_count(self):
+        impersonation_email = self.request.get('impersonationEmail')
         self.response.set_status(200, 'OK')
