f08bd1b5c1ccb57d01ba43bd7617f40e5ebbf5e4
bobmacneal <bob.macneal@workingcompany.com>
2016-08-05 10:13:29 -0500

    Add device management handler

--- handlers/reports/chrome_device_management_handler.py
+++ handlers/reports/chrome_device_management_handler.py
@@ -1,5 +1,8 @@
 from webapp2 import RequestHandler
 
+from restler.serializers import json_response
+from strategy import CHROME_OS_DEVICE_STRATEGY
+from workflow.interrogate_chrome_os_device import interrogate_chrome_os_devices
 from workflow.interrogate_chrome_os_device_by_mac_address import interrogate_chrome_os_device_by_mac_address
 
 __author__ = 'Bob MacNeal <bob.macneal@agosto.com>'
@@ -20,6 +23,14 @@ class ChromeDeviceManagementHandler(RequestHandler):
 
     def get_list(self):
         impersonation_email = self.request.get('impersonationEmail')
+        if impersonation_email:
+            devices = interrogate_chrome_os_devices(impersonation_email=impersonation_email)
+            for device in devices:
+                print(device.get('serial_number') + ';' + device.get('org_unit_path'))
+            json_response(self.response, devices, strategy=CHROME_OS_DEVICE_STRATEGY)
+        else:
+            message = 'Missing information in request body.'
+            return self.response.set_status(400, message)
         self.response.set_status(200, 'OK')
 
     def get_count(self):
