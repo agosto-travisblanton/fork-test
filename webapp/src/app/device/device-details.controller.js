37ce3066a9192fdcdaa0bede1db8df5b6859bd33
Daniel Ternyak <daniel.ternyak@agosto.com>
2016-08-08 14:33:55 -0500

    fix layout bug with image uploader

--- webapp/src/app/device/device-details.controller.js
+++ webapp/src/app/device/device-details.controller.js
@@ -17,7 +17,7 @@ function DeviceDetailsCtrl($log,
                            $scope) {
   "ngInject";
 
-  let vm = this;
+  const vm = this;
   vm.tenantKey = $stateParams.tenantKey;
   vm.deviceKey = $stateParams.deviceKey;
   vm.fromDevices = $stateParams.fromDevices === "true";
@@ -31,6 +31,7 @@ function DeviceDetailsCtrl($log,
   [vm.startTime, vm.endTime] = DateManipulationService.createFormattedStartAndEndDateFromToday(30);
   vm.enrollmentEvents = [];
   vm.logoChange = false;
+  vm.OVERLAY_TYPES = ["TIME", "DATE", "DATETIME", "LOGO"]
 
   $scope.$watch('selectedFile.length', function (newVal, oldVal) {
     if ($scope.selectedFile && $scope.selectedFile[0]) {
@@ -175,12 +176,6 @@ function DeviceDetailsCtrl($log,
     });
   };
 
-  vm.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-  'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-  'WY').split(' ').map(function (state) {
-    return {abbrev: state};
-  });
-
   vm.onGetDeviceFailure = function (response) {
     ToastsService.showErrorToast('Oops. We were unable to fetch the details for this device at this time.');
     let errorMessage = `No detail for device_key #${vm.deviceKey}. Error: ${response.status} ${response.statusText}`;
@@ -233,6 +228,8 @@ function DeviceDetailsCtrl($log,
       }), response => vm.onGetDeviceFailure(response));
     })
   }
+
+  // vm.
 
 
   vm.onSaveDevice = function () {
