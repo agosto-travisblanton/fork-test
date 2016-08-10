e8d8ba1979f7c228cdac9082975c5d7e1e1d2815
Daniel Ternyak <daniel.ternyak@agosto.com>
2016-08-10 14:47:38 -0500

    add try excepts everywhere

--- handlers/image_handler.py
+++ handlers/image_handler.py
@@ -1,6 +1,7 @@
 from google.appengine.ext import ndb
 from agar.sessions import SessionRequestHandler
 from models import Image
+import logging
 import json
 from ndb_mixins import KeyValidatorMixin
 from restler.serializers import json_response
@@ -8,8 +9,16 @@ from restler.serializers import json_response
 
 class ImageHandler(SessionRequestHandler, KeyValidatorMixin):
     def get(self, tenant_urlsafe_key):
-        tenant_key = ndb.Key(urlsafe=tenant_urlsafe_key).get().key
-        images = Image.get_by_tenant_key(tenant_key)
+        try:
+            tenant = ndb.Key(urlsafe=tenant_urlsafe_key).get()
+        except Exception, e:
+            logging.exception(e)
+        if tenant is None:
+            status = 404
+            message = 'Unrecognized tenant with key: {0}'.format(tenant)
+            return self.response.set_status(status, message)
+
+        images = Image.get_by_tenant_key(tenant.key)
         return json_response(
             self.response, [
                 {
@@ -20,6 +29,15 @@ class ImageHandler(SessionRequestHandler, KeyValidatorMixin):
         )
 
     def get_image_by_key(self, image_urlsafe_key):
+        try:
+            image = ndb.Key(urlsafe=image_urlsafe_key).get()
+        except Exception, e:
+            logging.exception(e)
+        if image is None:
+            status = 404
+            message = 'Unrecognized image with key: {0}'.format(image)
+            return self.response.set_status(status, message)
+
         image_entity = ndb.Key(urlsafe=image_urlsafe_key).get()
         return json_response(
             self.response, {
@@ -30,9 +48,17 @@ class ImageHandler(SessionRequestHandler, KeyValidatorMixin):
 
     def post(self, tenant_urlsafe_key):
         request_json = json.loads(self.request.body)
-        tenant_key = ndb.Key(urlsafe=tenant_urlsafe_key).get().key
         svg_rep = request_json.get("svg_rep")
         name = request_json.get("name")
+
+        try:
+            tenant = ndb.Key(urlsafe=tenant_urlsafe_key).get()
+        except Exception, e:
+            logging.exception(e)
+        if tenant is None:
+            status = 404
+            message = 'Unrecognized tenant with key: {0}'.format(tenant)
+            return self.response.set_status(status, message)
 
         if not name or name == '':
             return json_response(self.response, {
@@ -46,7 +72,7 @@ class ImageHandler(SessionRequestHandler, KeyValidatorMixin):
                 "message": "Missing svg_rep"
             }, status_code=400)
 
-        image_entity = Image.create(svg_rep=svg_rep, name=name, tenant_key=tenant_key)
+        image_entity = Image.create(svg_rep=svg_rep, name=name, tenant_key=tenant.key)
 
         if image_entity:
             json_response(self.response, {
