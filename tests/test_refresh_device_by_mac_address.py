5fe818d9c5420e4aa21fabc228e3be80ab2935dd
bobmacneal <bob.macneal@workingcompany.com>
2016-08-16 15:33:11 -0500

    Fix test

--- tests/test_refresh_device_by_mac_address.py
+++ tests/test_refresh_device_by_mac_address.py
@@ -10,12 +10,12 @@ from workflow.refresh_device_by_mac_address import refresh_device_by_mac_address
 class TestRefreshDeviceByMacAddress(BaseTest):
     SKYKIT_COM_CUSTOMER_ID = 'C04c2u0lg'
     ADMIN_ACCOUNT_TO_IMPERSONATE = 'administrator@skykit.com'
-    TESTING_DEVICE_ID = '6daf712b-7a65-4450-abcd-45027a47a716'
+    TESTING_DEVICE_ID = 'bca3c43b-0e9f-43b6-9028-c90ad432b80f'
     ORG_UNIT_DEPLOYED = '/SKD Automated Test/SKD Automated Deployed'
     ORG_UNIT_DISTRIBUTOR = '/SKD Automated Test/SKD Automated Distributor'
     CHROME_DEVICE_DOMAIN = 'dev.agosto.com'
     DISTRIBUTOR_NAME = 'agosto'
-    IMPERSONATION_EMAIL = 'administrator@skykit.com'
+    IMPERSONATION_EMAIL = 'skykit.api@dev.agosto.com'
 
     def setUp(self):
         super(TestRefreshDeviceByMacAddress, self).setUp()
@@ -36,7 +36,7 @@ class TestRefreshDeviceByMacAddress(BaseTest):
                                     domain_key=self.domain_key,
                                     active=True)
         self.tenant_key = self.tenant.put()
-        self.mac_address = '54271e4af1e7'
+        self.mac_address = 'd0df9a904b99'
         self.device = ChromeOsDevice.create_managed(tenant_key=self.tenant_key,
                                                     gcm_registration_id='8d70a8d78a6dfa6df76dfasd',
                                                     mac_address=self.mac_address)
