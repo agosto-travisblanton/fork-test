888444856b77c53f2371c2ae08dbe28ed94194dd
Daniel Ternyak <daniel.ternyak@agosto.com>
2016-10-20 14:32:14 -0500

    finish tests

--- routes.py
+++ routes.py
@@ -524,10 +524,9 @@ application = WSGIApplication(
         # /content-manager/ routes secured by shared_secret
         ############################################################
         Route(r'/api/v1/content-manager/devices/<device_urlsafe_key>',
-              handler='handlers.tenant_organization_units_handler.TenantOrganizationUnitsHandler',
-              name='organization-units-list',
-              handler_method='get_ou_list',
-              methods=['GET']
+              handler='handlers.content_manager_handler.ChromeOsDeviceHandler',
+              name='update-content-manager',
+              methods=['PUT']
               ),
 
         ############################################################
